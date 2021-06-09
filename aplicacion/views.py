94573225a710968013fb4c6027b7c1056af3c956
lvicencio <lvicencio@gmail.com>
2021-06-09 18:52:10 -0400

    commit

--- aplicacion/views.py
+++ aplicacion/views.py
@@ -1,6 +1,10 @@
+from django.core import paginator
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Producto
 from .forms import ContactoForm, ProductoForm
+from django.contrib import messages
+from django.core.paginator import Paginator
+from django.http import Http404
 # Create your views here.
 
 def home(request):
@@ -38,7 +42,7 @@ def agregar_producto(request):
         formulario = ProductoForm(data=request.POST, files=request.FILES)
         if formulario.is_valid():
             formulario.save()
-            data["mensaje"] = "Producto Registrado con Exito"
+            messages.success(request, "Producto Registrado con Exito")
         else:
             data["form"] = formulario
 
@@ -48,8 +52,19 @@ def agregar_producto(request):
 
 def listar_productos (request):
     productos = Producto.objects.all()
+
+    page = request.GET.get('page', 1)
+
+    try:
+        paginator = Paginator(productos, 2)
+        productos = paginator.page(page)
+    except:
+        raise Http404
+
+
     data = {
-        'productos': productos
+        'entity': productos,
+        'paginator': paginator
     }
     return render(request, 'app/producto/listar.html', data)
 
@@ -67,6 +82,7 @@ def editar_producto(request, id):
         formulario = ProductoForm(data=request.POST, instance=producto,files=request.FILES)
         if formulario.is_valid():
             formulario.save()
+            messages.success(request, "Editado de manera correcta!!")
             return redirect(to="listar_producto")
         else:
             data["form"] = formulario
@@ -76,4 +92,5 @@ def editar_producto(request, id):
 def eliminar_producto(request, id):
     producto = get_object_or_404(Producto, id=id)
     producto.delete()
+    messages.success(request, "Producto Eliminado Correctamente!!")
     return redirect(to="listar_producto")
